da2aa557e6cd67f935e6b16eb7b459c360cb6083
tujh028@yandex.ru <tujh028@yandex.ru>
2020-06-29 02:43:58 +0500

    Добавьте файлы проекта.

---
+++ programm/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace programm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+
+            int login = Convert.ToInt16(textBox1.Text);
+            int password = Convert.ToInt16(textBox2.Text);
+
+            bool ad = login == 123 && password == 123 ? true : false;
+                 
+            if (ad == true)
+            {
+                //this.Visible =false;               
+                form2.Show();
+            }
+            else
+            MessageBox.Show("Введите верный пароль или логин", "Ошибка");
+        }
+    }
+}
---
+++ programm/Form2.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace programm
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }        
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            RegistryKey Key1 = Registry.LocalMachine.OpenSubKey(@"HARDWARE\DESCRIPTION\System");//чтение реестра
+            string iden = (string)Key1.GetValue("Identifier");
+            string sys = (string)Key1.GetValue("SystemBiosDate");
+            string vid = (string)Key1.GetValue("VideoBiosDate");
+            Key1.Close();
+            string key1 = iden + sys + vid;            
+
+            RegistryKey currentUserKey = Registry.CurrentUser;//чтение программы
+            RegistryKey Key = currentUserKey.OpenSubKey(@"Software\Programm_reestr\Settings");
+            string key2 = Key.GetValue("KEY").ToString();
+            Key.Close();
+
+            
+            label2.Text=key1;
+            label3.Text= key2;
+
+            if(key1 != key2)
+            {
+                MessageBox.Show("Обратитесь к разработчику программы", "Ошибка");
+            }
+            else
+                MessageBox.Show("Программа работает", "Ура");           
+        }
+    }
+}
